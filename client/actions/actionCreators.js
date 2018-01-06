e4165edf426800e5dd9601082d9eab632dc1ed31
Timothy Ebiuwhe <timothy_ebiuwhe@live.com>
2018-01-06 23:31:40 +0100

    Wired App component to root router

--- client/actions/actionCreators.js
+++ client/actions/actionCreators.js
@@ -1,11 +1,11 @@
-export increment = index => {
+export function increment(index){
     return {
         type: 'INCREMENT_LIKES',
         index
     };
 }
 
-export addComment = (postId, author, comment) => {
+export function addComment(postId, author, comment){
     return {
         type: 'ADD_COMMENT',
         postId,
@@ -14,7 +14,7 @@ export addComment = (postId, author, comment) => {
     };
 }
 
-export removeComment = (postId, index) => {
+export function removeComment(postId, index){
     return {
         type: 'REMOVE_COMMENT',
         postId,
--- client/instadux.js
+++ client/instadux.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Route, Router, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
 
 import css from './styles/style.styl';
 
-import Main from './components/Main';
+import App from './components/App';
 import PhotoGrid from './components/PhotoGrid';
 import Single from './components/Single';
+import store, { history } from './store';
 
 const router = (
-    <Router history={browserHistory}>
-        <Route path="/" component={Main}>
-            <IndexRoute component={PhotoGrid}></IndexRoute>
-            <Route path="/view/:postId" component={Single}></Route>
-        </Route>
-    </Router>
+    <Provider store={store}>
+        <Router history={history}>
+            <Route path="/" component={App}>
+                <IndexRoute component={PhotoGrid}></IndexRoute>
+                <Route path="/view/:postId" component={Single}></Route>
+            </Route>
+        </Router>
+    </Provider>
 )
 
 render(router, document.getElementById('root'));
