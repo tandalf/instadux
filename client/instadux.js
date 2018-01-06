5405eb46addf0af2721211b762fa3b802debe34b
Timothy Ebiuwhe <timothy_ebiuwhe@live.com>
2018-01-06 18:55:05 +0100

    Routed components wrapped by Main component

--- client/instadux.js
+++ client/instadux.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Route, Router, IndexRoute, browserHistory } from 'react-router';
 
 import css from './styles/style.styl';
 
 import Main from './components/Main';
+import PhotoGrid from './components/PhotoGrid';
+import Single from './components/Single';
 
-render(<Main/>, document.getElementById('root'));
+const router = (
+    <Router history={browserHistory}>
+        <Route path="/" component={Main}>
+            <IndexRoute component={PhotoGrid}></IndexRoute>
+            <Route path="/view/:postId" component={Single}></Route>
+        </Route>
+    </Router>
+)
+
+render(router, document.getElementById('root'));
