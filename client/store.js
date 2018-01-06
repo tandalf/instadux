6edb024427fda6dc5508336ad27ebe5a48820f35
Timothy Ebiuwhe <timothy_ebiuwhe@live.com>
2018-01-06 20:05:52 +0100

    Created boiler plate code for the redux store

---
+++ client/store.js
@@ -0,0 +1,18 @@
+import { createStore } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router'; 
+
+import { comments } from './data/comments';
+import { posts } from './data/posts';
+import { rootReducer } from './reducers/index';
+
+const defaultState = {
+    posts,
+    comments
+};
+
+const store = createStore(rootReducer, defaultState);
+
+export const history = syncHistoryWithStore(browserHistory, store);
+
+export default store;
