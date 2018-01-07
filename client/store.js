669bd2a7c3d4e0d072f4a58dddb8c7a20859b4d0
Timothy Ebiuwhe <timothy_ebiuwhe@live.com>
2018-01-07 11:49:15 +0100

    Properly imported posts and comments data into store module

--- client/store.js
+++ client/store.js
@@ -2,8 +2,8 @@ import { createStore } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router'; 
 
-import { comments } from './data/comments';
-import { posts } from './data/posts';
+import comments from './data/comments';
+import posts from './data/posts';
 import rootReducer from './reducers/index';
 
 const defaultState = {
