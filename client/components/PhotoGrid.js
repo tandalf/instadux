474d15cd8de4dfc90bd21cae2d573a664b2c7639
Timothy Ebiuwhe <timothy_ebiuwhe@live.com>
2018-01-07 11:48:33 +0100

    Use new Photo component in PhotoGrid component

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+import Photo from './Photo';
+
 class PhotoGrid extends React.Component {
     constructor(props){
         super(props);
@@ -9,7 +11,7 @@ class PhotoGrid extends React.Component {
     render(){
         return (
             <div className="photo-grid">
-                PhotoGrid Placeholder
+                {this.props.posts.map( (post, i) => <Photo {...this.props} key={i} i={i} post={post}/> )}
             </div>
         )
     }
