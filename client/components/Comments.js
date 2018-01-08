1cf109f9373e90001a85ff8be019cbefa46d2267
Timothy Ebiuwhe <timothy_ebiuwhe@live.com>
2018-01-08 13:55:37 +0100

    Comments component created

---
+++ client/components/Comments.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Comments extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div>
+                Comment!!!
+            </div>
+        )
+    }
+}
+
+export default Comments;
