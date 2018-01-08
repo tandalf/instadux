86cef0a47294b7f919e3c2eb2f102acbc60a250b
Timothy Ebiuwhe <timothy_ebiuwhe@live.com>
2018-01-08 13:55:16 +0100

    Basic Single component for rendering a single image and its details

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+import Photo from './Photo';
+import Comments from './Comments';
+
 class Single extends React.Component {
     constructor(props){
         super(props);
     }
 
     render(){
+        const posts = this.props.posts;
+        const params = this.props.params;
+        const i = posts.findIndex(post => post.code == params.postId);
+        const post = posts[i];
         return (
             <div className="single-photo">
-                Single Photo Placeholder
+                <Photo {...this.props} key={i} i={i} post={post}/>
+                <Comments />
             </div>
         )
     }
